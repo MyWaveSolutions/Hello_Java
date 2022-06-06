400caa3793c88d65ab9260342d544d67c55ad8e7
J.G. Schneider | MyWave Solutions <info@mywave-solutions.nl>
2022-06-06 11:45:43 +0200

    fixed error
    Mistyped system

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
-public static void main(String[] args) {  
-// Prints "Hello, world" in the terminal window.
-Stystem.out.println("Hello, world");
-}
+   public static void main(String[] args) {  
+      // Prints "Hello, world" in the terminal window.
+      System.out.println("Hello, world");
+   }
 }
